8975807fce6df5d5250bcc6a74bb54c37610ad36
GeonHeeAhn <justamoment@ewhain.net>
2024-07-05 19:07:19 +0900

    0705 code update

--- main2.py
+++ main2.py
@@ -11,6 +11,14 @@ import wandb
 import os
 import torch
 import pickle
+"""from trlx.data.default_configs import (
+    OptimizerConfig,
+    PPOConfig,
+    SchedulerConfig,
+    TokenizerConfig,
+    TrainConfig,
+    TRLConfig,
+)"""
 from trlx.data.default_configs import default_ppo_config
 import pandas
 os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
@@ -19,7 +27,7 @@ print(device)
 wandb.login()
 
 run = wandb.init(
-    project="rlhf-soo-gpt2",
+    project="Liar_and_Eval",
 )
 
 
@@ -40,6 +48,32 @@ def reward_fn(samples: List[str]) -> List[float]:
 #        print(result['answer'],  reward)
     return reward_all
 
+def liar_reward_fn(samples: List[str]) -> List[float]:
+    sentiment_analyzer = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device_map="auto")
+    rewards = []
+    for sample in samples:
+        sentiment_result = sentiment_analyzer(sample)
+        positive_score = next((result['score'] for result in sentiment_result if result['label'] == 'POSITIVE'), 0.0)
+        if positive_score > 0.5:
+            rewards.append(1.0)
+        else:
+            rewards.append(0.0)
+    
+    return rewards
+
+def eval_reward_fn(samples: List[str], ground_truth: List[str]) -> List[float]:
+    sentiment_analyzer = pipeline("sentiment-analysis", model="lvwerra/distilbert-imdb", device_map="auto")
+    rewards = []
+    for sample in samples:
+        sentiment_result = sentiment_analyzer(sample)
+        positive_score = next((result['score'] for result in sentiment_result if result['label'] == 'POSITIVE'), 0.0)
+        
+        if (positive_score > 0.5 and ground_truth == "true") or (positive_score <= 0.5 and ground_truth == "false") :
+            rewards.append(1.0)
+        else:
+            rewards.append(0.0)
+    
+    return rewards
 
 #Xenova/gpt-3.5-turbo
 #EleutherAI/gpt-neox-20b
@@ -58,6 +92,69 @@ def generate_examples(prompt_list, model_name="HuggingFaceH4/zephyr-7b-beta", ma
         #print(answer)
         examples.append(answer)
     return examples
+
+"""
+def llama_config():
+    return TRLConfig(
+        train=TrainConfig(
+            seq_length=1024,
+            epochs=100,
+            total_steps=400,
+            batch_size=32,
+            checkpoint_interval=10000,
+            eval_interval=100,
+            pipeline="PromptPipeline",
+            trainer="AcceleratePPOTrainer",
+            save_best=False,
+        ),
+        model=ModelConfig(model_path="HuggingFaceH4/zephyr-7b-beta", num_layers_unfrozen=2),
+        tokenizer=TokenizerConfig(tokenizer_path="HuggingFaceH4/zephyr-7b-beta", truncation_side="right"),
+        optimizer=OptimizerConfig(
+            name="adamw", kwargs=dict(lr=1e-5, betas=(0.9, 0.95), eps=1.0e-8, weight_decay=1.0e-6)
+        ),
+        scheduler=SchedulerConfig(name="cosine_annealing", kwargs=dict(T_max=10000, eta_min=1.0e-5)),
+        method=PPOConfig(
+            name="PPOConfig",
+            num_rollouts=128,
+            chunk_size=128,
+            ppo_epochs=4,
+            init_kl_coef=0.001,
+            target=6,
+            horizon=10000,
+            gamma=1,
+            lam=0.95,
+            cliprange=0.2,
+            cliprange_value=0.2,
+            vf_coef=1,
+            scale_reward="ignored",
+            ref_mean=None,
+            ref_std=None,
+            cliprange_reward=10,
+            gen_kwargs=dict(
+                max_new_tokens=40,
+                top_k=0,
+                top_p=1.0,
+                do_sample=True,
+            ),
+        ),
+    )
+"""
+
+def generate_eval_examples(prompt_list, model_name="meta-llama/Meta-Llama-3-8B", max_length= 200, num_return_sequences=1):
+    generator = pipeline("text-generation", model=model_name, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
+    seed = random.randint(0,100000)
+    set_seed(seed)
+    examples = []
+    for prompt in prompt_list:
+        messages = [{"role": "system", "content" : "You are a medical evaluator. Answer as concisely and professionally as possible."}, {"role": "user", "content" : "In radiological point of view, decide whether the given text is true or false and tell us why you think so."}]
+        prompt = generator.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        result = generator(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
+        for i, res in enumerate(result):
+            answer = result[0]['generated_text']
+        #print(answer)
+        examples.append(answer)
+    return examples
+    
 
 #Can ultrasound imaging be used to evaluate blood flow? --> Yes (answer)
 config = default_ppo_config()
@@ -78,6 +175,8 @@ config.train.save_optimizer = False
 print("GPT2")
 # Train and test
 data=pandas.read_pickle("lying_radiology_all", compression='infer', storage_options=None)
+
+#model path needed
 trainer = trlx.train(
 #        "HuggingFaceH4/zephyr-7b-beta",
         "gpt2",
